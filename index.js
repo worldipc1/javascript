d99bc3ed187d48816086bcb0ac46ed72c463f210
worldipc1 <worldipc1@live.com>
2023-11-29 19:33:53 -0300

    inicio

---
+++ index.js
@@ -0,0 +1,76 @@
+// ejercicio 1 algoritmo con un condicional
+
+let number = parseFloat(prompt("ingresa un numero cualquiera"));
+function determinarSigno() {
+
+    if (number > 0) {
+        console.log("El número es positivo");
+        alert("el numero es positivo")
+    } else if (number < 0) {
+        console.log("El número es negativo");
+        alert("el numero es negativo")
+    } else {
+        console.log("El número es cero");
+        alert("el numero es 0")
+    }
+}
+determinarSigno();
+
+
+// ejercicio 2 crear un algoritmo utilizando un ciclo
+
+
+for (let i = 0; i < 5; i++) {
+
+    let numeroPar = 2 * i;
+    console.log("Número par:", numeroPar);
+}
+
+// simulador interactivo: Convertidor de monedas
+
+let ingresarMonto = parseFloat(prompt("ingresar el monto a convertir"));
+
+let dolar = 0.0028;
+let yuanes = 0.020;
+let lira = 0.0022;
+let euro = 0.0025;
+let resultado
+
+function cambio() {
+
+    let cambioDeMoneda = prompt("ingresar  para Dolar D para Lira esterlina L para Yuanes Y para Euro E ");
+
+    switch (cambioDeMoneda.toLocaleUpperCase()) {
+        case 'D':
+            let resultado = ingresarMonto * dolar;
+            console.log("la conversion ingresada es de " + resultado + " Dolares")
+            alert ("la conversion ingresada es de " + resultado + " Dolares")
+            break;
+        case 'L':
+            let resultado2 = ingresarMonto * lira;
+            console.log("la conversion ingresada es de " + resultado2 + " Liras Esterlinas")
+            alert ("la conversion ingresada es de " + resultado2 + " Liras Esterlinas")
+            break;
+
+        case 'Y':
+            let resultado3 = ingresarMonto * yuanes;
+            console.log("la conversion ingresada es de " + resultado3 + " Yuanes")
+            alert ("la conversion ingresada es de " + resultado3 + " Yuanes")
+            break;
+
+        case 'E':
+            let resultado4 = ingresarMonto * euro;
+            console.log("la conversion ingresada es de " + resultado4 + " Euros")
+            alert ("la conversion ingresada es de " + resultado4 + " Euros")
+            break;
+        default:
+            console.log("Opcion no valida. Por favor, ingrese D,L,Y o E")
+            cambio()
+    }
+
+}
+cambio()
+
+
+
+
